6c2ce10317d790603ccad54e5abb5f1c6934d057
Drake Boulianne <dboul63@gmail.com>
2024-01-06 16:01:14 -0700

    added base and home templates, rendered home template in home route

--- website/views.py
+++ website/views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 # when navigated to views route the 'home' function will run
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 
 
 
